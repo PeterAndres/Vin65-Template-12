77540ed5b531b09e48e3064f94a76dfa15555197
Carson Bain <33740139+CarsonBain@users.noreply.github.com>
2019-04-16 10:53:53 -0700

    Update for new photo gallery tools

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -129,7 +129,77 @@ var v65 = {
 };
 
 //Photogallery Plugin and Equalize Plugin
-;(function($,undefined){$.fn.v65PhotoGallery=function(options){var defaults={galleryId : $("#pagePhotoGallery").attr("v65jsphotogalleryid"),galleryHeight : $("#pagePhotoGallery").attr("v65jsphotogalleryheight"),galleryWidth : $("#pagePhotoGallery").attr("v65jsphotogallerywidth"),timestamp : "×tamp="+ new Date().getTime(),effect:'fade',slices:15, animSpeed:500,pauseTime:5000, startSlide:0, directionNav:true,directionNavHide:true,controlNav:true},gallery=$(this),settings=$.extend(defaults, options);gallery.html("").css({"height":settings.galleryHeight,"width":settings.galleryWidth,"overflow":"hidden"});$.ajax({type: "GET",url: "/index.cfm?method=pages.showPhotoGalleryXML&photogalleryid="+settings.galleryId+defaults.timestamp,dataType: "xml",success: function(xml){var images="";$(xml).find('img').each(function(){var location='/assets/images/photogallery/images/large/',photo=$(this).attr('src'),caption=$(this).attr('caption'),url=$(this).attr('link');if (url===undefined){images +='<img src="'+location+photo+'" title="'+caption+'"/>';}else{images +='<a href="'+url+'"><img src="'+location+photo+'" title="'+caption+'"/></a>';}});gallery.append(images);},complete: function(){gallery.nivoSlider({effect:settings.effect,slices:settings.slices,animSpeed:settings.animSpeed,pauseTime:settings.pauseTime,startSlide:settings.startSlide,directionNav:settings.directionNav,directionNavHide:settings.directionNavHide,controlNav:settings.controlNav});}});};$.fn.equalize=function(length){for(var i=0;i < this.length;i+=length){var elems=this.slice(i, i+length),equalizeArray=[];for(j=0;j < length;j++){equalizeArray.push(elems.eq(j).height());}var height=Math.max.apply( Math, equalizeArray);elems.css('min-height', height);}return this;};})(jQuery);
+;
+(function($, undefined) {
+	$.fn.v65PhotoGallery = function(options){
+		var defaults = {
+			galleryId : $("#pagePhotoGallery").attr("v65jsphotogalleryid"),
+			galleryHeight : null,
+			galleryWidth : null,
+			timestamp : "&timestamp="+ new Date().getTime()
+		},
+		gallery = $(this),
+		settings = $.extend(defaults, options);
+		gallery.html("").css({
+			"height":settings.galleryHeight,
+			"width":settings.galleryWidth,
+			"overflow":"hidden"
+		});
+		$.ajax({
+	    		type: "GET",
+			url: "/index.cfm?method=pages.showPhotoGalleryXML&photogalleryid="+settings.galleryId+defaults.timestamp,
+			dataType: "xml",
+			success: function(xml) {
+				var slides = "";
+				$(xml).find('img').each(function() {
+					var location = '/assets/images/photogallery/images/large/',
+						photo = $(this).attr('src'),
+						caption = $(this).attr('caption'),
+						title = $(this).attr('title'),
+						url = $(this).attr('link');
+						if (url === undefined) {
+						var	image = '<img alt="'+title+'" src="'+location+photo+'" title="'+caption+'"/>';
+						} else{
+						var	image = '<a href="'+url+'"><img alt="'+title+'" src="'+location+photo+'" title="'+caption+'"/></a>';
+						}
+						slides += image;
+				});
+				gallery.append(slides);
+			},
+			complete: function(){
+	   			gallery.slick({
+						arrows: settings.arrows, //Show the arrow navigation
+						autoplay: settings.autoplay, //Does the carousel autoplay or not
+						autoplaySpeed: settings.autoplaySpeed, // Adjust the transition speed between images. Value is in milliseconds
+						centerMode: settings.centerMode, //Enables centered view with partial prev/next slides. Use with odd numbered slidesToShow counts.
+            centerPadding: settings.centerPadding, //Side padding when in center mode (px or %)
+						dots: settings.dots, //Show the dot navigation for each image
+						fade: settings.fade, //Add a fade effect between image transitions
+            slidesToShow: settings.slidesToShow, //How may slides to show at once
+						slidesToScroll: settings.slidesToScroll //How many slides to scroll at once
+				});
+				$('#pagePhotoGallery .slick-slide img').each(function(){ 
+					if ($(this).attr('title')){
+						var slideCaption = $(this).attr('title');
+						$(this).parent('div').addClass('has-caption').append('<div class="slidecaption">' + slideCaption + '</div>');
+					}
+				});
+	   		}
+	   	});
+	};
+    $.fn.equalize = function(length) {
+        for (var i = 0; i < this.length; i += length) {
+            var elems = this.slice(i, i + length),
+                equalizeArray = [];
+            for (j = 0; j < length; j++) {
+                equalizeArray.push(elems.eq(j).height());
+            }
+            var height = Math.max.apply(Math, equalizeArray);
+            elems.css('min-height', height);
+        }
+        return this;
+    };
+})(jQuery);
 v65.home.init();
 v65.global.init();
 v65.page.initPhotoGallery();
